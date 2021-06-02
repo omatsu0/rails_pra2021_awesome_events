9b4e146ac88474e7cde16aa80693f1deae8403a0
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-06-02 23:06:36 +0900

    add rescue to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate
   helper_method :logged_in?, :current_user
 
+  rescue_from Exception,with: :error500
+  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
+
   private
 
   def logged_in?
